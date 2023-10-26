870162bb218bcc812880b45a593038a1dfc056c3
Guilherme <guilherme.klein99@gmail.com>
2023-10-26 09:23:23 -0300

    Update tokens

--- Etapa1/tokens.h
+++ Etapa1/tokens.h
@@ -1,29 +1,35 @@
 #ifndef TOKENS_H
 #define TOKENS_H
 
-#define KW_BYTE       256
-#define KW_INT        257
-#define KW_FLOAT      259
-#define KW_IF         261
-#define KW_THEN       262
-#define KW_ELSE       263
-#define KW_LOOP       264
-#define KW_LEAP       265
-#define KW_READ       266
-#define KW_RETURN     267
-#define KW_PRINT      268
-#define OPERATOR_LE   270
-#define OPERATOR_GE   271
-#define OPERATOR_EQ   272
-#define OPERATOR_DIF  273
-#define OPERATOR_OR   274
-#define OPERATOR_AND  275
-#define OPERATOR_NOT  276
-#define TK_IDENTIFIER 280
-#define LIT_INTEGER   281
-#define LIT_FLOAT     282
-#define LIT_CHAR      285
-#define LIT_STRING    286
-#define TOKEN_ERROR   290
+/*
+ * Compiladores - etapa1 - tokens.h - semestre 2023/2 - Guilherme Klein Kern
+ */
+
+#define KW_CHAR           256
+#define KW_INT            257
+#define KW_FLOAT          258
+
+#define KW_CODE           260
+
+#define KW_IF             261
+#define KW_ELSE           263
+#define KW_WHILE          264
+#define KW_INPUT          266
+#define KW_PRINT          267
+#define KW_RETURN         268
+
+#define OPERATOR_LE       270
+#define OPERATOR_GE       271
+#define OPERATOR_EQ       272
+#define OPERATOR_DIF      273
+
+#define TK_IDENTIFIER     280
+
+#define LIT_INT           281
+#define LIT_REAL          282
+#define LIT_CHAR          285
+#define LIT_STRING        286
+
+#define TOKEN_ERROR       290
 
 #endif
